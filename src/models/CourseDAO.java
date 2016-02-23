41ff14b9bbd292ba1331709116c7d2e37d2e1ef4
HoshijiroS <lynettedanielle.chan@gmail.com>
2016-02-23 16:05:22 +0800

    a lot of updates

--- src/models/CourseDAO.java
+++ src/models/CourseDAO.java
@@ -21,44 +21,44 @@ public class CourseDAO {
     
     public ArrayList<Course> GetAllCourses(){
         try
-    {
-      // create our mysql database connection
-      String myUrl = "jdbc:mysql://localhost/advanse";
-      Connection conn = DriverManager.getConnection(myUrl, "root", "");
-       
-      // our SQL SELECT query. 
-      // if you only need a few columns, specify them by name instead of using "*"
-      String query = "SELECT * FROM courses";
- 
-      // create the java statement
-      Statement st = conn.createStatement();
-       
-      // execute the query, and get a java resultset
-      ResultSet rs = st.executeQuery(query);
-     
-      // iterate through the java resultset
-      while (rs.next())
-      {
-        //String courseCode, int units, int maxNumberOfStudents, int numEnrollees
-        String courseCode = rs.getString("courseCode");
-        int units = rs.getInt("units");
-        int maxStudent = rs.getInt("maxStudent");
-        int numEnrollees = rs.getInt("numOfEnrollees");
-        Course c = new Course(courseCode, units, maxStudent, numEnrollees);
-        courses.add(c);
-         
-        // print the results
-        //System.out.format("%s, %s, %s, %s, %s, %s\n", id, firstName, lastName, dateCreated, isAdmin, numPoints);
-      }
-      st.close();
-    }
-    catch (Exception e)
-    {
-      System.err.println("Got an exception! ");
-      System.err.println(e.getMessage());
-    }
-        
-    return courses;
+        {
+          // create our mysql database connection
+          String myUrl = "jdbc:mysql://localhost/advanse";
+          Connection conn = DriverManager.getConnection(myUrl, "root", "");
+
+          // our SQL SELECT query. 
+          // if you only need a few columns, specify them by name instead of using "*"
+          String query = "SELECT * FROM courses";
+
+          // create the java statement
+          Statement st = conn.createStatement();
+
+          // execute the query, and get a java resultset
+          ResultSet rs = st.executeQuery(query);
+
+          // iterate through the java resultset
+          while (rs.next())
+          {
+            //String courseCode, int units, int maxNumberOfStudents, int numEnrollees
+            String courseCode = rs.getString("courseCode");
+            int units = rs.getInt("units");
+            int maxStudent = rs.getInt("maxStudent");
+            int numEnrollees = rs.getInt("numOfEnrollees");
+            Course c = new Course(courseCode, units, maxStudent, numEnrollees);
+            courses.add(c);
+
+            // print the results
+            //System.out.format("%s, %s, %s, %s, %s, %s\n", id, firstName, lastName, dateCreated, isAdmin, numPoints);
+          }
+          st.close();
+        }
+        catch (Exception e)
+        {
+          System.err.println("Got an exception! ");
+          System.err.println(e.getMessage());
+        }
+
+        return courses;
     }
     
     public ArrayList<String> getEnrolledCourse(int id){
--- src/models/Student.java
+++ src/models/Student.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package models;
 
 import java.util.ArrayList;
@@ -48,67 +47,10 @@ public class Student {
       this.courses = courses;
    }
    
-   public boolean addCourse(String s){
-       if(this.courses.size() > 9){
-           return false;
-       }
-       return true;
-   }
-=======
-package models;
-
-import java.util.ArrayList;
-
-public class Student {
-   private String name;
-   private int id;
-   private String password;
-   private ArrayList<Course> courses;
-
-   public Student(String name, int id, String password, ArrayList<Course> courses) {
-      this.name = name;
-      this.id = id;
-      this.password = password;
-      this.courses = courses;
-   }
-
-   public String getName() {
-      return name;
-   }
-
-   public void setName(String name) {
-      this.name = name;
-   }
-
-   public int getId() {
-      return id;
-   }
-
-   public void setId(int id) {
-      this.id = id;
-   }
-
-   public String getPassword() {
-      return password;
-   }
-
-   public void setPassword(String password) {
-      this.password = password;
-   }
-
-   public ArrayList<Course> getCourses() {
-      return courses;
-   }
-
-   public void setCourses(ArrayList<Course> courses) {
-      this.courses = courses;
-   }
-   
    public boolean addCourse(Course course){
        if(this.courses.size() > 9){
            return false;
        }
        return true;
    }
->>>>>>> 8092dfafc706cb0ef17fe8be7a8fbec630973f28
 }
