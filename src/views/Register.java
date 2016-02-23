a17f10b30c13952eddbece3e2a7903de9c58fbf1
jed1337 <j.caychingco@Yahoo.com>
2016-02-23 17:34:16 +0800

    Commit

--- src/views/Register.java
+++ src/views/Register.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package views;
 
 import dao.StudentDAO;
 import java.util.ArrayList;
 import models.Course;
-import models.Student;
-/**
- *
- * @author Nagate
- */
+
 public class Register extends javax.swing.JPanel {
 
     /**
